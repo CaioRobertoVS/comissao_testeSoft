3d9d49f42b713bc5b079378dc43720e3b08185c0
Caio Roberto Vieira Santos <brl.caio1991@outlook.com>
2020-04-03 01:00:53 -0400

    pequena alteração

--- bin/main.dart
+++ bin/main.dart
@@ -10,10 +10,10 @@ void main() {
 
   var cliente = Cliente(nome: 'João');
 
-  var pedro = Funcionario(nome: 'Pedro', cargo: gerente);
-  var caio = Funcionario(nome: 'Caio', cargo: supervisor);
-  var venda1 = Venda(valor: 100, cliente: cliente, funcionario: pedro);
-  var venda2 = Venda(valor: 100, cliente: cliente, funcionario: caio);
+  var caio = Funcionario(nome: 'Pedro', cargo: gerente);
+  var pedro = Funcionario(nome: 'Caio', cargo: supervisor);
+  var venda1 = Venda(valor: 100, cliente: cliente, funcionario: caio);
+  var venda2 = Venda(valor: 100, cliente: cliente, funcionario: pedro);
 
   var gerenteVenda = gerente.comissao(venda1).toStringAsFixed(2);
   var supervisorVenda = supervisor.comissao(venda2).toStringAsFixed(2);
