87793c60b6c643b86a3f0add73e6a022d737a3c3
Caio Roberto Vieira Santos <brl.caio1991@outlook.com>
2020-04-03 15:36:31 -0400

    alteração no teste do cliente

--- test/cliente_test.dart
+++ test/cliente_test.dart
@@ -3,14 +3,19 @@ import 'package:test/test.dart';
 
 void main() {
   group('Cliente', () {
-    final cliente = Cliente(nome: 'Caio');
+    final cliente1 = Cliente(nome: 'Caio');
     final cliente2 = Cliente(nome: 'Eli');
     test("deve ter nome igual 'Caio'", () {
-      expect(cliente.nome, 'Caio');
+      expect(cliente1.nome, 'Caio');
     });
 
     test("deve ter nome igual 'Eli'", () {
       expect(cliente2.nome, 'Eli');
+    });
+
+    test("nome deve ser vazio ''", () {
+      final cliente = Cliente();
+      expect(cliente.nome, '');
     });
   });
 }
